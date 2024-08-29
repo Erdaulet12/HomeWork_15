cb087328e878ea05c58f5b5b99685a387255ea91
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-08-29 20:05:49 +0500

    views.py

--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -4,13 +4,13 @@ from django.views.generic import DetailView
 
 
 class UserListView(ListView):
-    model = User
-    template_name = 'user_list.html'
-    context_object_name = 'users'
+	model = User
+	template_name = 'user_list.html'
+	context_object_name = 'users'
 
 
 class UserDetailView(DetailView):
-    model = User
-    template_name = 'user_detail.html'
-    context_object_name = 'user'
-    pk_url_kwarg = 'user_id'
+	model = User
+	template_name = 'user_detail.html'
+	context_object_name = 'user'
+	pk_url_kwarg = 'user_id'
